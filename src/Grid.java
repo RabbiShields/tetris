35f81142b56d70f86ccc207d3c6077dd39ab6e82
Rabbi Shields <yishaishields@gmail.com>
2019-07-01 11:41:38 -0400

    collision detection now detects side impacts. pieces colliding with the side of another frozen piece. so collision detection now detects left and right boundry impacts, floor and frozen pieces from the bottem and side.

--- src/Grid.java
+++ src/Grid.java
@@ -5,7 +5,7 @@ public class Grid {
     private int yPos = 2;
     private int[][] blockGrid;
     private boolean colide = false;
-
+    boolean projectionScanDone = false;
 
     Grid() {
         grid = new int[30][35];
@@ -16,18 +16,90 @@ public class Grid {
         blockGrid = arrayInitializer(blockGrid);
     }
 
-    private int[][] arrayInitializer(int[][] grid) {
-        for (int i = 0; i < grid.length - 1; i++) {
-            for (int j = 0; j < grid[i].length - 1; j++) {
-                grid[i][j] = 0;
+    void startGame() {
+        Blocks block = new Blocks();
+        blockGrid = block.generateAblock();
+    }
+
+    void move(int xMove, int yMove) {
+        colide = false;
+        System.out.println("(x" + xPos + "," + yPos + ")");
+        a:
+        for (int i = xPos; i < xPos + 4; i++) {
+            for (int j = yPos; j < yPos + 4; j++) {
+                //System.out.print("(" + i + "," + j + ")");
+                if (xMove == 1) {
+                    if (frozenPeicesGrid[i+xMove][j] == 1 & blockGrid[i - xPos][j - yPos] == 1) {
+                        colide = true;
+                        break a;
+                    }
+                    if (blockGrid[i - xPos][j - yPos] == 1 & i > 24) {
+                        colide = true;
+                        break a;
+                    }
+                }
+                if (xMove == -1) {
+                    if (frozenPeicesGrid[i+xMove][j] == 1 & blockGrid[i - xPos][j - yPos] == 1) {
+                        colide = true;
+                        break a;
+                    }
+                    if (blockGrid[i - xPos][j - yPos] == 1 & i < 6) {
+                        colide = true;
+                        break a;
+                    }
+                }
+                if (yMove == 1) {
+                    if (blockGrid[i - xPos][j - yPos] == 1 & j > 25) {
+                        colide = true;
+                        blockIsDoneFalling();
+                        break a;
+                    }
+                }
+                if (frozenPeicesGrid[i][j + 1] == 1 & blockGrid[i - xPos][j - yPos] == 1) {
+                    colide = true;
+                    blockIsDoneFalling();
+                    break a;
+                }
             }
         }
+        if (!colide) {
+            xPos += xMove;
+            yPos += yMove;
+            mergeGrids();
+        }
+    }
+
+    int[][] getGrid() {
         return grid;
     }
 
-    public void startGame() {
-        Blocks block = new Blocks();
-        blockGrid = block.generateAblock();
+    void pieceProjection() {
+        projectionScanDone = false;
+        int tempYPos = yPos;
+        while (!projectionScanDone) {
+            b: for (int i = xPos; i < xPos + 4; i++) {
+                for (int j = tempYPos; j < tempYPos + 4; j++) {
+                    //System.out.print("(i" + i + "," + j + ")");
+                    //System.out.print(projectionScanDone);
+                    if (frozenPeicesGrid[i][j + 1] == 1 & blockGrid[i - xPos][j - tempYPos] == 1) {
+                        projectionScanDone = true;
+                        break b;
+                    }
+                    if (blockGrid[i - xPos][j - tempYPos] == 1 & j > 25) {
+                        projectionScanDone = true;
+                        break b;
+                    }
+                }
+            }
+            if  (!projectionScanDone)tempYPos++;
+        }
+        if (projectionScanDone) {
+            for (int i = xPos; i < xPos + 4; i++) {
+                for (int j = tempYPos; j < tempYPos + 4; j++) {
+                    if (blockGrid[i - xPos][j - tempYPos] == 1) grid[i][j] = blockGrid[i - xPos][j - tempYPos];
+                }
+            }
+        }
     }
 
     private void blockIsDoneFalling() {
@@ -58,73 +130,12 @@ public class Grid {
                         blockGrid[i - xPos][j - yPos];
         }
     }
-
-    public void move(int xMove, int yMove) {
-        colide = false;
-        System.out.println("(x" + xPos + "," + yPos + ")");
-        for (int i = xPos; i < xPos + 4; i++) {
-            for (int j = yPos; j < yPos + 4; j++) {
-                System.out.print("(" + i + "," + j + ")");
-                if (frozenPeicesGrid[i][j + 1] == 1 & blockGrid[i - xPos][j - yPos] == 1) {
-                    colide = true;
-                    blockIsDoneFalling();
-                    break;
-                }
-                if (xMove == 1) {
-                    if (blockGrid[i - xPos][j - yPos] == 1 & i > 24) colide =
-                            true;
-                }
-                if (xMove == -1) {
-                    if (blockGrid[i - xPos][j - yPos] == 1 & i < 6) colide =
-                            true;
-                }
-
-                if (yMove == 1) {
-                    if (blockGrid[i - xPos][j - yPos] == 1 & j > 25) {
-                        colide = true;
-                        blockIsDoneFalling();
-                        break;
-                    }
-                }
+    private int[][] arrayInitializer(int[][] grid) {
+        for (int i = 0; i < grid.length - 1; i++) {
+            for (int j = 0; j < grid[i].length - 1; j++) {
+                grid[i][j] = 0;
             }
         }
-        if (!colide) {
-            xPos += xMove;
-            yPos += yMove;
-            mergeGrids();
-        }
-    }
-
-    public int[][] getGrid() {
         return grid;
-    }
-
-    public void pieceProjection() {
-        boolean projectionScanDone = false;
-        int tempYPos = yPos;
-        while (!projectionScanDone) {
-            a: for (int i = xPos; i < xPos + 4; i++) {
-                for (int j = tempYPos; j < tempYPos + 4; j++) {
-                    System.out.print("(i" + i + "," + j + ")");
-                    System.out.print(projectionScanDone);
-                    if (frozenPeicesGrid[i][j + 1] == 1 & blockGrid[i - xPos][j - tempYPos] == 1) {
-                        projectionScanDone = true;
-                        break a;
-                    }
-                    if (blockGrid[i - xPos][j - tempYPos] == 1 & j > 25) {
-                        projectionScanDone = true;
-                        break a;
-                    }
-                }
-            }
-            if  (!projectionScanDone)tempYPos++;
-        }
-        if (projectionScanDone) {
-            for (int i = xPos; i < xPos + 4; i++) {
-                for (int j = tempYPos; j < tempYPos + 4; j++) {
-                    if (blockGrid[i - xPos][j - tempYPos] == 1) grid[i][j] = blockGrid[i - xPos][j - tempYPos];
-                }
-            }
-        }
     }
 }
